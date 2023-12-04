a3e8c885c951c6b67fc44c7d3620ac7a1b3c1021
Joe <wenxing.cn@gmail.com>
2023-12-04 21:54:53 +0800

    add code

--- screen/home/home.js
+++ screen/home/home.js
@@ -5,7 +5,56 @@ Page({
    * Page initial data
    */
   data: {
-
+    contentIndex: 0,
+    imageUrls : [
+      "/asset/image/1.jpg",
+      "/asset/image/2.jpg",
+      "/asset/image/3.jpg"
+    ]
+  },
+  item0 () {
+    this.setData({
+      contentIndex:0,
+    })
+    wx.showLoading({
+      title: '加载中',
+    })
+    
+    setTimeout(function () {
+      wx.hideLoading()
+    }, 2000)
+  },
+  item1 () {
+    this.setData({
+      contentIndex:1,
+    })
+    wx.showLoading({
+      title: '加载中',
+    })
+    
+    setTimeout(function () {
+      wx.hideLoading()
+    }, 2000)
+  },
+  item2 () {
+    this.setData({
+      contentIndex:2,
+    })
+  },
+  item3 () {
+    this.setData({
+      contentIndex:3,
+    })
+  },
+  changeImage() {
+    console.log('changeImage')
+    this.setData({
+      imageUrls: [
+        "/asset/image/4.jpg",
+      "/asset/image/5.jpg",
+      "/asset/image/6.jpg"
+      ],
+    })
   },
   jump() {
     wx.switchTab({
